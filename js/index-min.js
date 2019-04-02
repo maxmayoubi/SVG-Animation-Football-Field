a510023623f0cd4790da1f449430830f94b60199
user44 <user44@intad.atton.ae>
2019-04-02 16:18:23 +0400

    js min file

---
+++ js/index-min.js
@@ -0,0 +1,1 @@
+﻿var $svg=$("svg").drawsvg({callback:function(){$("#logo").fadeIn(1e3)}});$svg.drawsvg("animate"),$(document).ready(function(){$("svg").fadeIn(300),$("#logo").css("top",$("svg").offset().top-20+"px"),$("#logo").css("left",$("svg").offset().left+"px")});
